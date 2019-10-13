5e148c8816d945d81db988b40cccfbcab014dddc
meghana <meghana.bairy@gmail.com>
2019-10-13 08:28:25 +0530

    Commit the changes as part of new feature

--- NewJavaProject/src/org/com/Test1.java
+++ NewJavaProject/src/org/com/Test1.java
@@ -4,7 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("say Hello ");
+		System.out.println("say Hello from Branch");
+		System.out.println("adding additonal line inthe next branch");
+		
 	}
 
 }
