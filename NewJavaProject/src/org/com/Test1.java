f8152b92e7f7cc05b109e896fee5a6da45ba5158
meghanabairy <meghana.bairy@gmail.com>
2019-10-13 09:53:55 +0530

    adding feature 1 to master

--- NewJavaProject/src/org/com/Test1.java
+++ NewJavaProject/src/org/com/Test1.java
@@ -6,7 +6,7 @@ public class Test1 {
 		// TODO Auto-generated method stub
 		//added as requetsed
 		System.out.println("completed the changes requested");
-		
+		System.out.println("adding feature 1 to the master");
 		System.out.println("say Hello from Branch");
 		System.out.println("adding additonal line inthe next branch");
 		
